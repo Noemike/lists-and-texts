6d493ca0cabad4817c0ec81bfc3f75264eebd329
Noemike <66320530+Noemike@users.noreply.github.com>
2020-06-08 17:52:04 +0200

    not finished

--- scripts.js
+++ scripts.js
@@ -1,6 +1,21 @@
-$('body').append(
-    '<h1>Hello World!</h1>'
-  );
+console.log('Hi,Noemi')
 
-let names=['Levi', 'Noni', 'Norbi', 'Anya' 'Apa' 'Baba'];
+let names=['Levi', 'Noni', 'Norbi', 'Anya', 'Apa', 'Baba'];
 
+/*$('body').append(
+  names.forEach
+)*/
+names.forEach(unicorn => {
+  console.log(unicorn);
+})
+
+$('ul').append([names])
+
+let additionalBlock = {
+  title: "Added with javascript",
+  text: "This block was added using JavaScript's jQuery library. How awesome!"
+};
+
+/*$('body').append('<h1>Added with javascript</h1>')
+
+$('body').append("<p>This block was added using JavaScript's jQuery library. How awesome!</p>")*/
